f472dcc67ec3d7c6e1527dd31d4322829c35308d
Your Name <you@example.com>
2024-10-21 17:42:22 +0900

    [pre-commit] init

--- include/DifferentiableImage/DifferentiableImage.h
+++ include/DifferentiableImage/DifferentiableImage.h
@@ -2,73 +2,76 @@
 #pragma once
 
 #include <array>
+#include <ostream>
 #include <per/core/prCameraModel.h>
+#include <sstream>
 #include <string>
 #include <vector>
-#include <ostream>
-#include <sstream>
 
 #include "ceres/ceres.h"
 #include <ceres/cubic_interpolation.h>
 
-#include <per/prRegularlySampledCPImage.h>
 #include <per/prFeaturesSet.h>
-#include <per/prPhotometricnnGMS.h>
 #include <per/prPerspective.h>
+#include <per/prPhotometricnnGMS.h>
+#include <per/prRegularlySampledCPImage.h>
 
+#include <visp/vpDisplayX.h>
 #include <visp/vpImage.h>
 #include <visp/vpImageIo.h>
 #include <visp/vpImageTools.h>
-#include <visp/vpDisplayX.h>
 
-class DifferentiableImage{
-    public:
-        DifferentiableImage() = default;
-        ~DifferentiableImage() = default;
+class DifferentiableImage
+{
+public:
+  DifferentiableImage() = default;
+  ~DifferentiableImage() = default;
 
-        void init(const std::string &path_des, const std::string &path_cur, bool enable_display = true, const std::string &mask = "");
-        double compute();
+  void init(const std::string & path_des,
+            const std::string & path_cur,
+            bool enable_display = true,
+            const std::string & mask = "");
+  double compute();
 
-    private:
-        vpImage<unsigned char> I_des;
-        vpImage<unsigned char> I_cur;
-        vpImage<unsigned char> I_mask;
-        bool hasMask;
-        bool enableDisplay;
+private:
+  vpImage<unsigned char> I_des;
+  vpImage<unsigned char> I_cur;
+  vpImage<unsigned char> I_mask;
+  bool hasMask;
+  bool enableDisplay;
 
-        //Desired PGM
-        vpImage<unsigned char> PGM_des_u;
+  // Desired PGM
+  vpImage<unsigned char> PGM_des_u;
 
-        vpImage<unsigned char> I_to_upsub;
-        vpImage<unsigned char> upsampled_subsampled_I_to_upsub;
+  vpImage<unsigned char> I_to_upsub;
+  vpImage<unsigned char> upsampled_subsampled_I_to_upsub;
 
-        vpImage<unsigned char> diff_u;
-        vpImage<unsigned char> PGM_cur_u;
-        
-        vpDisplayX disp_I_des;
-        vpDisplayX disp_I_cur;
-        vpDisplayX disp_PGM_des;
-        vpDisplayX disp_upsampled_subsampled_I_to_upsub;
-        vpDisplayX disp_diff_u;
-        vpDisplayX disp_PGM_cur;
+  vpImage<unsigned char> diff_u;
+  vpImage<unsigned char> PGM_cur_u;
 
-        unsigned int nbKernels;
-        double sigmaMin; // within a pixel 
-        double sigmaMax;//desired_Gray.cols/6.0 ; //the whole image
-        double sigma;
-        double sigmaStep;
+  vpDisplayX disp_I_des;
+  vpDisplayX disp_I_cur;
+  vpDisplayX disp_PGM_des;
+  vpDisplayX disp_upsampled_subsampled_I_to_upsub;
+  vpDisplayX disp_diff_u;
+  vpDisplayX disp_PGM_cur;
 
-        double dCostSigma;
-        double interpolated_sigma;
+  unsigned int nbKernels;
+  double sigmaMin; // within a pixel
+  double sigmaMax; // desired_Gray.cols/6.0 ; //the whole image
+  double sigma;
+  double sigmaStep;
 
-        std::vector<double> v_cost;
-        std::vector<double> v_sigma;
+  double dCostSigma;
+  double interpolated_sigma;
 
-        std::ostringstream s;
-        std::string filename;
+  std::vector<double> v_cost;
+  std::vector<double> v_sigma;
 
-        // Camera
-        prSensorModel *_sensor;
-        prCameraModel *_camera;
+  std::ostringstream s;
+  std::string filename;
 
+  // Camera
+  prSensorModel * _sensor;
+  prCameraModel * _camera;
 };
--- src/DifferentiableImageStandalone.cpp
+++ src/DifferentiableImageStandalone.cpp
@@ -1,39 +1,36 @@
-#include <DifferentiableImage/DifferentiableImage.h>
-#include "glog/logging.h"
 #include "gflags/gflags.h"
+#include "glog/logging.h"
+#include <DifferentiableImage/DifferentiableImage.h>
 
 DEFINE_string(desired, "", "File of the reference image");
 DEFINE_string(current, "", "File of the current image");
 DEFINE_string(mask, "", "File of the mask image");
 DEFINE_bool(display, true, "Enable/Disable display");
 
-int main(int argc, char **argv){
-    google::ParseCommandLineFlags(&argc, &argv, true);
-    google::InitGoogleLogging(argv[0]);
+int main(int argc, char ** argv)
+{
+  google::ParseCommandLineFlags(&argc, &argv, true);
+  google::InitGoogleLogging(argv[0]);
 
-    if(FLAGS_desired.empty()) {
-        std::cerr << "Please provide a reference image file name using -desired.\n";
-        return 1;
-    }
-    if(FLAGS_current.empty()) 
-    {
-        std::cerr << "No current image file name using -current.\n";
-        return 1;
-    }
+  if(FLAGS_desired.empty())
+  {
+    std::cerr << "Please provide a reference image file name using -desired.\n";
+    return 1;
+  }
+  if(FLAGS_current.empty())
+  {
+    std::cerr << "No current image file name using -current.\n";
+    return 1;
+  }
 
-    DifferentiableImage diff_image;
+  DifferentiableImage diff_image;
 
-    if(FLAGS_mask.empty()) 
-    {
-        diff_image.init(FLAGS_desired, FLAGS_current, FLAGS_display);
-    }
-    else{
-        diff_image.init(FLAGS_desired, FLAGS_current, FLAGS_display, FLAGS_mask);
-    }
+  if(FLAGS_mask.empty()) { diff_image.init(FLAGS_desired, FLAGS_current, FLAGS_display); }
+  else { diff_image.init(FLAGS_desired, FLAGS_current, FLAGS_display, FLAGS_mask); }
 
-    double lambda_result = diff_image.compute();
+  double lambda_result = diff_image.compute();
 
-    std::cout << "Computed lambda is : " << lambda_result << std::endl;
+  std::cout << "Computed lambda is : " << lambda_result << std::endl;
 
-    return 0;
+  return 0;
 }
